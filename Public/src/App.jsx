42c0f3387bbd8f03a01403613c43ed36096a689a
Eddy <eddy@Eddys-MBP.lan>
2021-03-30 19:38:28 -0400

    getting a simple interface and foundation set up for app

---
+++ Public/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends React.component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    return <div> Discord Bot </div>;
+  }
+}
